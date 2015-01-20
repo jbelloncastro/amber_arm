b2e0d8766d003bad62930f24b790c1d9e50a6d9c
jbelloncastro <jorge.bellon@est.fib.upc.edu>
2015-01-20 18:20:45 +0100

    Now errors in error correcting code are also taken into account.

--- hw/vlog/tb/ecc_tb/encoder_test.v
+++ hw/vlog/tb/ecc_tb/encoder_test.v
@@ -3,9 +3,10 @@ module encoder_test;
 reg write_enable;  
 reg [31:0] my_data;//data to test: 0xe3a02001
 reg [7:0] my_ecc;// ecc for this data: 0xf7
-reg [31:0] bitflip_mask;
+reg [39:0] bitflip_mask;
 
 wire [31:0] input_data;
+wire [7:0] input_ecc;
 wire [31:0] output_data;
 wire [7:0] ecc_syndrome;
 wire uncorrected;
@@ -16,21 +17,24 @@ begin
   my_data = 32'he3a02001;
   my_ecc = 8'hf7;
   write_enable = 1'b0;
-  bitflip_mask = 32'd75;
+  bitflip_mask = 40'h1;
 end
 
 genvar i;
 generate
   // Flip my_data bit values using bitflip_mask (only on read operations)
-  for( i = 0; i < 32; i=i+1) begin
-    assign input_data[i] = my_data[i] ^ (bitflip_mask[i] & ~write_enable);
+  for( i = 8; i < 40; i=i+1) begin
+    assign input_data[i-8] = my_data[i-8] ^ (bitflip_mask[i] & ~write_enable);
+  end
+  for( i = 0; i < 8; i=i+1) begin
+    assign input_ecc[i] = my_ecc[i] ^ (bitflip_mask[i] & ~write_enable);
   end
 endgenerate
 
 edc_generator generator (
   .o_ecc_syndrome         ( ecc_syndrome               ),  // To Corrector and ECC Memory
   .i_write_enabled        ( write_enable               ),  
-  .i_ecc                  ( my_ecc                     ),  // From ECC Memory
+  .i_ecc                  ( input_ecc                     ),  // From ECC Memory
   .i_data                 ( input_data                 )   // From multiplexor
 );
 
